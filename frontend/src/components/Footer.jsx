4cc0b34c2558af90beab34cfc0b403e13b96b1c1
it23377844 <it23377844@my.sliit.lk>
2025-12-26 22:40:03 +0530

    Fix: remove frontend submodule and add as normal folder

---
+++ frontend/src/components/Footer.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Footer() {
+  return (
+    <footer>
+      <p>
+        &copy; {new Date().getFullYear()} Greenery Tech (Pvt) Ltd. Industrial RO
+        Plants, Ice Machines &amp; Cold Rooms.
+      </p>
+    </footer>
+  );
+}
+
+export default Footer;
+
